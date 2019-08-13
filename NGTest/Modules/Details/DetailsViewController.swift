73476cf4c57e6ee8efd3be7e4b92a3b62e21e86b
Albie <unco182@gmail.com>
2019-08-12 21:10:24 -0400

    DetailsViewController is now a UITableViewController

--- NGTest/Modules/Details/DetailsViewController.swift
+++ NGTest/Modules/Details/DetailsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DetailsViewController: UIViewController {
+class DetailsViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

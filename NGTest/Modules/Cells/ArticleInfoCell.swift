07cd2311ea67d2697d0cdc035eae8774a5509b6a
Albie <unco182@gmail.com>
2019-08-12 21:09:27 -0400

    Create UI cells

---
+++ NGTest/Modules/Cells/ArticleInfoCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleInfoCell.swift
+//  NGTest
+//
+//  Created by Alban Pellegrini on 12/08/2019.
+//  Copyright © 2019 Nuglif. All rights reserved.
+//
+
+import UIKit
+
+class ArticleInfoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ NGTest/Modules/Cells/MediaCell.swift
@@ -0,0 +1,24 @@
+//
+//  MediaCell.swift
+//  NGTest
+//
+//  Created by Alban Pellegrini on 12/08/2019.
+//  Copyright © 2019 Nuglif. All rights reserved.
+//
+
+import UIKit
+
+class MediaCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

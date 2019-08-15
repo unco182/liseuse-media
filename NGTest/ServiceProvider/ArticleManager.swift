bbc1fa3cfa5140cde20618bd0aa38fa7f0b6ce0f
Albie <unco182@gmail.com>
2019-08-14 20:48:19 -0400

    Filter services

--- NGTest/ServiceProvider/ArticleManager.swift
+++ NGTest/ServiceProvider/ArticleManager.swift
@@ -42,5 +42,39 @@ class ArticleManager: NSObject {
             .mapObject(type: ArticleDetails.self)
             .asSingle()
     }
+    
+    func fetchChannelNames() -> [String] {
+        return fetchArticles().map { (article) -> String in
+            return article.channelName
+        }
+    }
+    
+    func fetchFilteredArticles(_ filter: FilterUICell) -> [Article] {
+        switch filter {
+
+        case .resetFilter:
+            return fetchArticles()
+        case .channelItem(_, _):
+            return fetchArticles().filter({ (article) -> Bool in
+                return filter == .channelItem(article.channelName, isSelected: false)
+            })
+        case .olderArticle(_):
+            return fetchArticles().sorted(by: { (article1, article2) -> Bool in
+                let df = DateFormatter()
+                df.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+                guard let article1ModificationDate = df.date(from: article1.modificationDate), let article2ModificationDate = df.date(from: article2.modificationDate) else { return false }
+                return article1ModificationDate > article2ModificationDate
+            })
+        case .newerArticle(_):
+            return fetchArticles().sorted(by: { (article1, article2) -> Bool in
+                let df = DateFormatter()
+                df.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+                guard let article1ModificationDate = df.date(from: article1.modificationDate), let article2ModificationDate = df.date(from: article2.modificationDate) else { return false }
+                return article1ModificationDate < article2ModificationDate
+            })
+
+        }
+
+    }
 
 }

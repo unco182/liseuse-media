47f3d01d6cda96caefcaec69605d2b2c23447fb2
Albie <unco182@gmail.com>
2019-08-14 20:48:28 -0400

    Filter Utils

--- NGTest/Utils/FilterUICell+Equatable.swift
+++ NGTest/Utils/FilterUICell+Equatable.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+func ==(a: FilterUICell, b: FilterUICell) -> Bool {
+    switch(a) {
+    case .resetFilter:
+        switch b {
+        case .resetFilter:
+            return true
+        case .channelItem(_, _), .olderArticle(_), .newerArticle(_):
+            return false
+        }
+    case .channelItem(let aChannelName, _):
+        switch b {
+        case .resetFilter, .olderArticle(_), .newerArticle(_) :
+            return false
+        case .channelItem(let bChannelName, _):
+            return bChannelName == aChannelName
+            
+        }
+    case .olderArticle(_):
+        switch b {
+        case .olderArticle:
+            return true
+        case .channelItem(_, _), .resetFilter, .newerArticle(_):
+            return false
+        }
+    case .newerArticle(_):
+        switch b {
+        case .newerArticle(_):
+            return true
+        case .channelItem(_, _), .olderArticle(_), .resetFilter:
+            return false
+        }
+        
+    }
+}
